c517cf221df5efdfe0119470ccbea8f26b4d69ca
Daniel Poveda <dlpoveda@gmail.com>
2021-03-02 15:20:51 -0500

    Chore: define db and tables

---
+++ server/db/models/product.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  category: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: 'other'
+  },
+  price: {
+    type: Sequelize.DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+})
+
+module.exports = Product
