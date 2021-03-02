e7c8bd75b53c669de0ea7d8b4c086f7dcb8b7f53
Jeremy Cook <jeremycookbiz@gmail.com>
2021-03-02 16:51:28 -0500

    11 redux store for products

---
+++ client/store/singleProduct.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+const SINGLE_PRDOUCT = 'SINGLE_PRODUCT'
+
+const initalState = {}
+
+export const singleProduct = (data) => ({
+  type: SINGLE_PRODUCT,
+  data,
+})
+
+export default function singleProductReducer(state = initialState, action) {
+  switch (action.type) {
+    case SINGLE_PRODUCT:
+      return action.data
+    default:
+      return state
+  }
+}
+
+export const fetchSingleProduct = (id) => {
+  return async (dispatch) => {
+    try {
+      const {data} = await axios.get(`/api/products/${id}`)
+      dispatch(fetchSingleProduct(data))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
