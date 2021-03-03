1f7754affcd1b88201c5d1c33a98181d7e6ef274
Daniel Poveda <dlpoveda@gmail.com>
2021-03-03 17:33:44 -0500

    Chore: added Cat and ProductOrder models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,6 +1,7 @@
 const User = require('./user')
 const Product = require('./product')
-
+const Cat = require('./cat')
+const ProductOrder = require('./productOrder')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -16,5 +17,7 @@ const Product = require('./product')
  */
 module.exports = {
   User,
-  Product
+  Product,
+  Cat,
+  ProductOrder
 }
