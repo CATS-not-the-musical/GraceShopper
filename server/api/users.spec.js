598b0b05427f498bbc256d4a7b784bf2e9da7a55
Yin Song <songniy@gmail.com>
2021-03-07 15:41:29 -0500

    fix:test spec

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -27,7 +27,7 @@ describe('User routes', () => {
       const res = await request(app)
         .get('/api/users')
         .expect(200)
-      expect(res.body).to.be.an('array')
+      expect(res.body).to.be.an('object')
     })
     // it('Get /api/cats', async () => {
     //   const res = await request(app)
