a6edd85c28dfa7b8c6f225f945f3fb68e5ad260f
Jeremy Cook <jeremycookbiz@gmail.com>
2021-03-09 16:12:46 -0500

    79 Conquered

--- client/components/OrderHistory.js
+++ client/components/OrderHistory.js
@@ -23,14 +23,26 @@ export class OrderHistory extends React.Component {
     } else {
       return (
         <div>
-          <h1>Order History</h1>
+          <h1>Order History</h1> <br />
+          <h1>Thank you for adopting!</h1> <br />
           {UserOrders.map(order => {
             return (
               <div key={order.id}>
-                <h1>Order Number{order.id}</h1>
+                <h1>
+                  Order Number: {order.id} <br /> Order Date:{' '}
+                  {order.createdAt.slice(0, 10)}
+                </h1>
                 {order.cats.map(cat => {
-                  return <div key={cat.id}>Cat No.{cat.id}</div>
+                  return (
+                    <h1 key={cat.id}>
+                      Name: {cat.firstName} {cat.lastName} <br />
+                      Quantity: {cat.productOrder.quantity} <br />
+                      Adoption Fee: ${cat.adoptionFee} <br />
+                      <img src={cat.image} /> <br />
+                    </h1>
+                  )
                 })}
+                <h1>__________________________</h1>
               </div>
             )
           })}
